005b7109f3437cb96dad5f98ffc8ae2da607f96a
aqib420 <aa06586@st.habib.edu.pk>
2026-03-12 01:08:10 +0500

    patient management system for creating crud apis, view patients is done

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI() #object created for fastapi class
 
-@app.get("/hey") #path endpoint defined route.
+@app.get("/") #path endpoint defined route.
 
 #now we createa  functions for this endpoint
 def hello():
-    return {'message: hello world'}
+    return {'message: Patient Management System API'}
 
 
 
 @app.get("/about")
 
 def about():
-    return{'message': 'This is my second endpoint for learning fastapi'}
+    return{'message': 'A fully functional api to manage your paitent record'}
+
+
+#loading patients.json file to load data
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    
+    return data
+
+# print(load_data())
+
+#view all patients
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
+#view a particular patient based on id
+
 
